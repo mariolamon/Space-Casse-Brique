e47010108b1b2cc2993ce2674384fb3f5c33238e
mariolamon <mario.vanhovelamon@gmail.com>
2019-02-01 16:12:57 +0100

    collide

--- master.js
+++ master.js
@@ -5,7 +5,7 @@ window.requestAnimFrame = (function() {
 		window.oRequestAnimationFrame				||
 		window.msRequestAnimationFrame			||
 	function (callback) {
-		return window.setTimeout(callback , 1000 / 60);
+		return window.setTimeout(callback , 1000 / 120);
 	});
 })();
 window.cancelRequestAnimFrame = (function() {
@@ -16,6 +16,7 @@ window.cancelRequestAnimFrame = (function() {
 		window.msCancelRequestAnimationFrame 			||
 		clearTimeout);
 })();
+
 let canvas = document.getElementById('canvas'),
 		ctx = canvas.getContext('2d'),
 		H = 600,
@@ -24,29 +25,81 @@ let canvas = document.getElementById('canvas'),
 		backgroundSpace = {},
 		mouse = {},
 		ball = {},
-		brick = [],
+		paddle = {},
+		stars = [],
+		mediumStars = [],
+		bigStars = [],
+		bricks = [],
+		tmpStars = 0,
+		speed = 4,
+		dx = 0,
+		dy = 0,
 		lvl = 0,
-		score = 0,
-		init;
+		life = 1,
+		score = 0;
 
 //canvas.addEventListener("mousemove", trackPosition, true);
 //canvas.addEventListener("mousedown", click, true);
 
 canvas.height = H;
 canvas.width = W;
-
-brick = [];
+dx = speed;
+dy = -speed;
+bricks = [
+	{x: 30, y: 30}, {x: 155, y: 30}, {x: 280, y: 30}, {x: 405, y: 30},
+	{x: 530, y: 30}, {x: 655, y: 30},	{x: 30, y: 60}, {x: 155, y: 60},
+	{x: 280, y: 60}, {x: 405, y: 60}, {x: 530, y: 60}, {x: 655, y: 60},
+	{x: 30, y: 90}, {x: 155, y: 90}, {x: 280, y: 90}, {x: 405, y: 90},
+	{x: 530, y: 90}, {x: 655, y: 90}, {x: 30, y: 120}, {x: 155, y: 120},
+	{x: 280, y: 120}, {x: 405, y: 120}, {x: 530, y: 120}, {x: 655, y: 120}
+];
+function brickDraw() {
+	ctx.beginPath();;
+	ctx.fillStyle = "#fff";
+	ctx.shadowColor = 'red';
+	ctx.shadowBlur = 30;
+	for(let i = 0; bricks[i]; i++)
+		ctx.fillRect(bricks[i].x, bricks[i].y, 115, 20);
+	ctx.closePath();;
+};
+ball = {
+	x: 400, y: 300, w: 10, h: 0,
+	draw: function() {
+		ctx.beginPath();;
+		ctx.fillStyle = "#fff";
+		ctx.shadowColor = 'red';
+		ctx.shadowBlur = 30;
+		ctx.fillStyle = "#fff";
+		ctx.arc(this.x, this.y, this.w, this.h, (Math.PI * 2), false);
+		ctx.fill();
+		ctx.closePath();;
+	}
+};
 backgroundSpace = {
 	w: 800, h: 600, x: 0, y:0,
 	draw: function() {
-		ctx.save();
+		ctx.beginPath();
+		ctx.fillStyle = "#000";
+		ctx.fillRect(this.x, this.y, this.w, this.h);
 		ctx.fillStyle = "#fff";
-		for(let i = 0; i < 100; i++) {
-			let x = Math.floor(Math.random() * Math.floor(this.w));
-			let y = Math.floor(Math.random() * Math.floor(this.h));
-			ctx.fillRect(x, y, 1, 1);
+		let i = 0, a, b;
+		for(let i = 0; i < 300; i++)
+			ctx.fillRect(stars[i].x, stars[i].y, 1, 1);
+		for(let i = 20; i < 70; i++)
+				ctx.fillRect(stars[i].x, stars[i].y, 2, 2);
+		for(let i = 0; i < 20; i++)
+			ctx.fillRect(stars[i].x, stars[i].y, 3, 3);
+		for(let i = 0; mediumStars[i]; i++) {
+			ctx.fillRect((mediumStars[i].x - 1), (mediumStars[i].y - 1), 3, 3);
+			ctx.fillRect(mediumStars[i].x, (mediumStars[i].y - 4), 1, 9);
+			ctx.fillRect((mediumStars[i].x - 2), mediumStars[i].y, 5, 1);
 		}
-		ctx.restore();
+		for(let i = 0; bigStars[i]; i++) {
+			ctx.fillRect((bigStars[i].x - 1), (bigStars[i].y - 2), 3, 5);
+			ctx.fillRect(bigStars[i].x, (bigStars[i].y - 5), 1, 11);
+			ctx.fillRect((bigStars[i].x - 4), bigStars[i].y, 9, 1);
+		}
+		ctx.closePath();
 	}
 };
 restartBtn = {
@@ -65,12 +118,37 @@ restartBtn = {
 	}
 };
 
+function createStars() {
+	for(let i = 0; i < 300; i++) {
+		let x = Math.floor(Math.random() * Math.floor(W));
+		let y = Math.floor(Math.random() * Math.floor(H));
+		stars.push({x: x, y: y});
+	}
+};
 function draw() {
 	backgroundSpace.draw();
+	brickDraw();
+	ball.draw();
 	update();
 };
 function update() {
 	if (life == 0) gameOver();
+	if (tmpStars == 300) {
+		mediumStars = [];
+		bigStars = [];
+		for(let i = 0; i < 5; i++) {
+			let tmp = Math.floor(Math.random() * Math.floor(300));
+			mediumStars.push({x: stars[tmp].x, y: stars[tmp].y});
+		}
+		for(let i = 0; i < 2; i++) {
+			let tmp = Math.floor(Math.random() * Math.floor(300));
+			bigStars.push({x: stars[tmp].x, y: stars[tmp].y});
+		}
+		tmpStars = 0;
+	} else tmpStars++;
+	ballCollide();
+	ball.x += dx;
+	ball.y += dy;
 };
 function updateScore() {
 	ctx.save();
@@ -78,11 +156,17 @@ function updateScore() {
 	ctx.font = "20px 'btn',arial,sans-serif";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Score: " + point, 20, 20 );
+	ctx.fillText("Score: " + score, 20, 20 );
 	ctx.restore();
 };
+function ballCollide() {
+	if(ball.x + dx > (W - ball.w) || ball.x + dx < ball.w)
+		dx = -dx;
+	if(ball.y + dy > (H - ball.w) || ball.y + dy < ball.w)
+		dy = -dy;
+};
 function animLoop() {
-	init = requestAnimFrame(animLoop);
+	let init = requestAnimFrame(animLoop);
 	draw();
 };
 function gameOver() {
@@ -97,3 +181,5 @@ function gameOver() {
 	over = 1;
 	restartBtn.draw();
 };
+createStars();
+animLoop();
